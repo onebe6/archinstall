e9de948102028b9867c89bb892be277dae633cb1
onebe6 <83883307+onebe6@users.noreply.github.com>
2021-12-10 00:44:32 +0100

    Add files via upload

---
+++ Basic_Setup.sh
@@ -0,0 +1,29 @@
+#! bin/bash
+
+pacman -S networkmanager grub
+
+systemctl enable NetworkManager
+
+grub-install $DISK
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Add a root password: "
+
+passwd
+
+nano /etc/locale.conf
+
+nano /etc/hostname
+
+ln -sf /usr/share/zoneinfo/Europe/Budapest etc/localtime
+
+read -p "Add a username:" USER
+
+useradd -mg wheel $USER
+
+echo "Add password for the user:"
+
+passwd $USER
+
+exit
