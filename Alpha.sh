805e36859a3c8b900af1d9736877dee6cb3d6adb
onebe6 <83883307+onebe6@users.noreply.github.com>
2021-12-10 19:50:35 +0100

    Add files via upload

--- Alpha.sh
+++ Alpha.sh
@@ -20,39 +20,8 @@ mkfs.ext4 $BOOT
 mkdir /mnt/boot
 mount /mnt/boot
 
-pacstrap /mnt base base-devel linux linux-firmware vim nano terminator 
-tft-inconsolata
+pacstrap /mnt base base-devel linux linux-firmware vim nano
 
-genfstab -U /mnt
-
-arch-chroot /mnt /bin/bash
-
-pacman -S networkmanager grub
-
-systemctl enable NetworkManager
-
-grub-install $DISK
-
-grub-mkconfig -o /boot/grub/grub.cfg
-
-echo "Add a root password: "
-
-passwd
-
-nano /etc/locale.conf
-
-nano /etc/hostname
-
-ln -sf /usr/share/zoneinfo/Europe/Budapest etc/localtime
-
-read -p "Add a username:" USER
-
-useradd -mg wheel $USER
-
-echo "Add password for the user:"
-
-passwd $USER
+genfstab -U /mnt >> /mnt/etc/fstab
 
 exit
-
-umount /mnt
--- Start.sh
+++ Start.sh
@@ -2,3 +2,5 @@
 
 bash Alpha.sh
 arch-chroot /mnt /root/archinstall/Basic_Setup.sh
+umount -R /mnt
+reboot
