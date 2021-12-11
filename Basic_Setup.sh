b2d4e65201e0ddbf163a1472c4ce9b472dc4ce70
onebe6 <83883307+onebe6@users.noreply.github.com>
2021-12-11 22:37:30 +0100

    Add files via upload

--- Basic_Setup.sh
+++ Basic_Setup.sh
@@ -15,8 +15,8 @@ read -p "which is your boot drive? (exemple: /dev/sda) " B_DRIVE
 if [[ -d "/sys/firmware/efi" ]]
 then
     grub-install --target=x86_64-efi  --efi-directory=/boot --bootloader-id=GRUB ${B_DRIVE}
-else
-    grub-install --target=i386-pc ${B_DRIVE}
+#else
+#    grub-install --target=i386-pc ${B_DRIVE}
 fi
 
 #grub-install
