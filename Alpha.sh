f0ceecae7df7c316946863dbb3ba35bf26bced77
onebe6 <83883307+onebe6@users.noreply.github.com>
2021-12-11 20:33:56 +0100

    Add files via upload

--- Alpha.sh
+++ Alpha.sh
@@ -2,15 +2,22 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-#pacman -S gptfdisk
+pacman -S gptfdisk
 
 lsblk
 
-read -p "which drive do you want to partition?: (exemple: /dev/sda/)" DISK
+read -p "which drive do you want to partition?: (exemple: /dev/sda)" DISK
 
-#sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
-#sgdisk -n 2::+100M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-#sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+)
+
+if [[ -d "/sys/firmware/efi" ]] then
+    sgdisk -n 1::+100M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
+    sgdisk -n 2::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+else
+    sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition
+    sgdisk -n 2::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+fi
+
 #if [[ ! -d "/sys/firmware/efi" ]]; then
 #    sgdisk -A 1:set:2 ${DISK}
 #xfi
@@ -19,7 +26,7 @@ read -p "which drive do you want to partition?: (exemple: /dev/sda/)" DISK
 #mkfs.ext4 -L "ROOT" "${DISK}p3" -f
 #mount -t ext4 "${DISK}p3" /mnt
 
-cfdisk $DISK
+#cfdisk $DISK
 
 lsblk
 
@@ -29,11 +36,19 @@ mkfs.ext4 $PARTITION
 
 mount $PARTITION /mnt
 
+lsblk
+
 read -p "which partition is your boot partition? (exemple: /dev/sda1)" BOOT
 
-mkfs.vfat -F32 $BOOT
-mkdir /mnt/boot
-mount $BOOT /mnt/boot
+if [[ -d "/sys/firmware/efi" ]] then
+    mkfs.vfat -F32 $BOOT
+    mkdir /mnt/boot
+    mount $BOOT /mnt/boot
+else
+    mkfs.ext4 $BOOT
+    mkdir /mnt/boot
+    mount $BOOT /mnt/boot
+fi
 
 #mount -t vfat -L EFIBOOT /mnt/boot/
 
